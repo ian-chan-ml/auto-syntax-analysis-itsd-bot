d3e630a4595f7282900d6589181558957a8fbf61
quanianitis <ichan@moneylion.com>
2023-11-30 00:51:57 +0800

    add syntax analysis google cloud api

--- main.py
+++ main.py
@@ -28,6 +28,68 @@ def message_hello(message, say):
         say(f"Keyword found, but no complete sentence detected, <@{message['user']}>.")
 
 
+def syntax_analysis(content):
+    client = language_v1.LanguageServiceClient()
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"content": content, "type_": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_syntax(
+        request={"document": document, "encoding_type": encoding_type}
+    )
+
+# Loop through tokens returned from the API
+    for token in response.tokens:
+        # Get the text content of this token. Usually a word or punctuation.
+        text = token.text
+        print(f"Token text: {text.content}")
+        print(f"Location of this token in overall document: {text.begin_offset}")
+        # Get the part of speech information for this token.
+        # Part of speech is defined in:
+        # http://www.lrec-conf.org/proceedings/lrec2012/pdf/274_Paper.pdf
+        part_of_speech = token.part_of_speech
+        # Get the tag, e.g. NOUN, ADJ for Adjective, et al.
+        print(
+            "Person: {}".format(
+                language_v1.PartOfSpeech.Tag(part_of_speech.person).name
+            )
+        )
+        # Get the voice, e.g. ACTIVE or PASSIVE
+        print(
+            "Aspect: {}".format(
+                language_v1.PartOfSpeech.Voice(part_of_speech.aspect).name
+            )
+        )
+        # Get the tense, e.g. PAST, FUTURE, PRESENT, et al.
+        print(
+            "Mood: {}".format(
+                language_v1.PartOfSpeech.Tense(part_of_speech.mood).name
+            )
+        )
+        # See API reference for additional Part of Speech information available
+        # Get the lemma of the token. Wikipedia lemma description
+        # https://en.wikipedia.org/wiki/Lemma_(morphology)
+        print(f"Lemma: {token.lemma}")
+        # Get the dependency tree parse information for this token.
+        # For more information on dependency labels:
+        # http://www.aclweb.org/anthology/P13-2017
+        dependency_edge = token.dependency_edge
+        print(f"Head token index: {dependency_edge.head_token_index}")
+        print(
+            "Label: {}".format(
+                language_v1.DependencyEdge.Label(dependency_edge.label).name
+            )
+        )
+
 
 # Start your app
 if __name__ == "__main__":
